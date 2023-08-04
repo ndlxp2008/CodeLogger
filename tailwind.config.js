915e49df540f4caec956dc4321dee7ef443c3671
ahole <ndlxp2008@outlook.com>
2023-08-04 13:10:20 +0800

    build: 去除多余代码

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./**/*.{html,js}",
     './src/**/*.{vue,scss,css,tsx,ts,js,jsx}',
     './components/**/*.{vue,scss,css,tsx,ts}',
     './layouts/**/*.{vue,scss,css,tsx,ts}',
