2c8db795c69339121fda3dce190a1b4244cecf4b
ahole <ndlxp2008@outlook.com>
2023-08-13 07:53:20 +0800

    feat: pinia测试

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './style.css';
+import 'element-plus/dist/index.css';
 
-// import 'element-plus/dist/index.css';
 import { createApp } from 'vue';
 
 import App from './App.vue';
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,18 @@
-import { createPinia } from 'pinia';
+import { createPinia, defineStore } from 'pinia';
 
 const pinia = createPinia();
 export default pinia;
+
+export const useAppStore = defineStore('userStore', {
+  state: () => ({
+    testCount: 1,
+  }),
+  actions: {
+    add() {
+      return this.testCount++;
+    },
+    minus() {
+      return this.testCount--;
+    },
+  },
+});
