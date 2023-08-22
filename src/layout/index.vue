a191ee67122febeac3124c32c39fa5b13733a1b6
ahole <ndlxp2008@outlook.com>
2023-08-22 09:29:13 +0800

    feat: layout基础结构

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,15 +1,47 @@
+<script setup lang="ts">
+import NavBar from '@/layout/NavBar.vue';
+import SideBar from '@/layout/SideBar.vue';
 
+const route = useRoute()
+onMounted(() => { })
+</script>
 <template>
   <div class="app-container">
-    <div>
+    <SideBar class="left" />
+    <div class="right">
+      <NavBar />
+      <div class="main bg-blue-600">
+        <!-- TODO 切换动画 -->
+        <!-- <Transition name="fade" mode="out-in" appear>
+          <router-view />
+        </Transition> -->
         <router-view :key="route.path" />
-        <h1 class="bg-slate-500">容器页</h1>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
-const route = useRouter()
-</script>
+<style scoped lang="scss">
+.app-container {
+  user-select: none;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
 
-<style scoped></style>
+  .right {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+
+    // 内容区域
+    .main {
+      margin: 10px 20px 20px;
+      border-radius: 4px;
+      position: relative;
+      overflow-y: auto;
+      flex: 1;
+    }
+  }
+}
+</style>
